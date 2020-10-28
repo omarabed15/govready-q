860be61ba884e70a37ecd0b4e17469e7392e4ff9
davidpofo <dampofo@umd.edu>
2020-10-28 13:54:07 -0400

    Added show_modal_error to attachment ajax request to display attachment error. Added unit test assert for validate_file_extension.

--- discussion/tests.py
+++ discussion/tests.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Permission
 from django.utils.crypto import get_random_string
 from django.contrib.auth.models import Permission
 
+from discussion.validators import validate_file_extension
 from siteapp.models import User, ProjectMembership, Organization, Portfolio
 from siteapp.tests import SeleniumTest, var_sleep
 
@@ -169,6 +170,8 @@ class DiscussionTests(SeleniumTest):
 
         # Test that the image actually exists.
         imageFile = img.get_attribute('src')
+        imageFileValidation = validate_file_extension(imageFile)
+        self.assertIsNone(imageFileValidation)
 
         result = self.browser.execute_script("""var http = new XMLHttpRequest();
             http.open('HEAD', '{}', false);
